6b76ad0593fea3ebf0af975bc6360dfd57db23f3
luisrom661 <luisrom@unicauca.edu.co>
2024-01-17 16:49:52 -0500

    Add UpdateStockRequestDto and Update method to StockController

--- Controllers/StockController.cs
+++ Controllers/StockController.cs
@@ -45,5 +45,25 @@ namespace api.Controllers
             _context.SaveChanges();
             return CreatedAtAction(nameof(GetById), new { id = stockModel.Id }, stockModel.ToStockDto());
         }
+
+        [HttpPut]
+        [Route("{id}")]
+        public IActionResult Update([FromRoute] int id, [FromBody] UpdateStockRequestDto updateDto)
+        {
+            var stockModel = _context.Stocks.FirstOrDefault(x => x.Id == id);
+            if(stockModel == null)
+            {
+                return NotFound();
+            }
+            stockModel.Symbol = updateDto.Symbol;
+            stockModel.CompanyName = updateDto.CompanyName;
+            stockModel.Purchase = updateDto.Purchase;
+            stockModel.MarketCap = updateDto.MarketCap;
+            stockModel.LastDiv = updateDto.LastDiv;
+            stockModel.Industry = updateDto.Industry;
+
+            _context.SaveChanges();
+            return Ok(stockModel.ToStockDto());
+        }
     }
 }
